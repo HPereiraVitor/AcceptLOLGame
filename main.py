debcfcf2b5750ce792c50eb18036fc239b67e2ab
HPereiraVitor <vitor.pereira11@fatec.sp.gov.br>
2023-06-06 12:04:43 -0300

    Preventing to repetitions

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import pyautogui
 
-while True:
-    # Encontre a posição do botão na tela
-    button_location = pyautogui.locateOnScreen('image/button_aceitar.png')
-    # Verifique se o botão foi encontrado
-    if button_location is not None:
-        print('BUTTON FOUND...')
+if __name__ == '__main__':
+    print('SEARCHING...')
 
-        # Obtenha as coordenadas do centro do botão
-        buttonPoint = pyautogui.center(button_location)
-        button_x, button_y = buttonPoint
+    while True:
+        # Encontre a posição do botão na tela
+        button_location = pyautogui.locateOnScreen('image/button_aceitar.png')
+        # Verifique se o botão foi encontrado
+        if button_location is not None:
+            print('BUTTON FOUND...')
 
-        # Mova o cursor do mouse para o centro do botão
-        pyautogui.moveTo(button_x, button_y)
+            # Obtenha as coordenadas do centro do botão
+            buttonPoint = pyautogui.center(button_location)
+            button_x, button_y = buttonPoint
 
-        # Clique no botão
-        pyautogui.click()
+            # Mova o cursor do mouse para o centro do botão
+            pyautogui.moveTo(button_x, button_y)
 
-        break
-    else:
-        print('SEARCHING...')
+            # Clique no botão
+            pyautogui.click()
+
+            break
